d3cf105a6899714471b915cb947a2dc23e4f6a79
msh8030 <155610555+msh8030@users.noreply.github.com>
2024-02-06 19:51:06 +0900

    "수정본"

--- src/views/component/Wrapper/animation.js
+++ src/views/component/Wrapper/animation.js
@@ -1,11 +1,11 @@
 export const pageEffect = {
     initial: {
-      opacity: 0
+      opacity: 0.7
     },
     in: {
       opacity: 1
     },
     out: {
-      opacity: 0
+      opacity: 0.7
     }
   };
--- src/views/social/community/Community.js
+++ src/views/social/community/Community.js
@@ -116,7 +116,17 @@ function Community() {
 
 
                         <div class="col-lg-12 col-sm-12">
+
                             <div class="blog-single-box upper">
+                            <div class="blog-left">
+                                <div className="blog-icon">
+                                </div>
+                                    <div className='blog-description'>
+                                        <a href="#">Jo-dong-hun</a>
+                                        <a href="#">서울시 강남구 서초동 서초대로</a>
+                                        <span>January 27, 2023</span>
+                                    </div>
+                                </div>
                                 <div class="blog-thumb">
                                     <img src={require('../../../assets/images/blog-0.jpg')} alt=""/>
                                     <div class="blog-btn">
@@ -125,21 +135,16 @@ function Community() {
                                 </div>
                                 <div class="blog-content">
 
-                                    <div class="blog-left">
-                                        <div className="blog-icon">
-                                        </div>
-                                        <div className='blog-description'>
-                                            <a href="#">Jo-dong-hun</a>
-                                            <a href="#">서울시 강남구 서초동 서초대로</a>
-                                            <span>January 27, 2023</span>
-                                        </div>
-                                    </div>
+
 
                                     <h2><a href="blog-details.html">이번에 새로 산 차.</a></h2>
                                     
                                     <p>Nostra dapibus varius et semper semper rutrum ad risus felis eros. Cursus libero viverra tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula egestas commodo massa. Lorem non sit vivamus convallis elit mollis. Viverra sodales feugiat
                                         dsfdsafadsfadsfadsfdasdsfdsafadsfads
                                     </p>
+
+
+
                                     <div class="blog-button">
                                         <a href="#">read more</a>
                                         <div className="blog-button-container">
@@ -260,7 +265,6 @@ function Community() {
                             </div>
                         </div>
                         </div>
-
 
                         
                         <div class="col-lg-4 col-md-6 col-sm-12">
