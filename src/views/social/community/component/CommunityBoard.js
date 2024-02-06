a10633f7c0e6994506e69e5d13b44285e072cb56
msh8030 <155610555+msh8030@users.noreply.github.com>
2024-02-06 19:56:17 +0900

    "게시글 추가"

--- src/views/social/community/component/CommunityBoard.js
+++ src/views/social/community/component/CommunityBoard.js
@@ -1,7 +1,18 @@
+import './CommunityBoard.css';
+
 function CommunityBoard() {
     return (
         <div class="col-lg-12 col-sm-12">
         <div class="blog-single-box upper">
+            <div class="blog-left bl1" style={{padding:"60px 0px"}}>
+                <div className="blog-icon bi1">
+                </div>
+                <div className='blog-description'>
+                    <a href="#">Jo-dong-hun</a>
+                    <a href="#">서울시 강남구 서초동 서초대로</a>
+                    <span>January 27, 2023</span>
+                </div>
+            </div>
             <div class="blog-thumb">
                 <img src={require('../../../../assets/images/blog-0.jpg')} alt=""/>
                 <div class="blog-btn">
@@ -9,17 +20,6 @@ function CommunityBoard() {
                 </div>
             </div>
             <div class="blog-content">
-
-                <div class="blog-left">
-                    <div className="blog-icon">
-                    </div>
-                    <div className='blog-description'>
-                        <a href="#">Jo-dong-hun</a>
-                        <a href="#">서울시 강남구 서초동 서초대로</a>
-                        <span>January 27, 2023</span>
-                    </div>
-                </div>
-
                 <h2><a href="blog-details.html">이번에 새로 산 차.</a></h2>
                 
                 <p>Nostra dapibus varius et semper semper rutrum ad risus felis eros. Cursus libero viverra tempus netus diam vestibulum lorem tincidunt congue porta. Non ligula egestas commodo massa. Lorem non sit vivamus convallis elit mollis. Viverra sodales feugiat
