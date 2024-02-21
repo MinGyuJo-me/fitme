1979ec2af13dd5cd870ae013094c0fc08f4a5dfb
Jin-Ogeu <powa963@gmail.com>
2024-02-21 09:29:39 +0900

    챗봇 커밋이 안올라가네????

--- src/views/component/chatBot/ChatBot.js
+++ src/views/component/chatBot/ChatBot.js
@@ -3,17 +3,15 @@ import './Chatbot.css';
 import 'material-symbols';
 import axios from 'axios'; //npm install axios
 
-
-var id = null;
 var ipAddress = '192.168.0.110';
 
 function ChatBot() {
   const [userMessage, setUserMessage] = useState(null);
-  const inputInitHeight = 40; // 텍스트 영역의 초기 높이를 설정하세요
+  const inputInitHeight = 40; // 텍스트 영역의 초기 높이를 설정
 
 
   const createChatLi = (message, className) => {
-    // 전달된 메시지와 클래스 이름으로 채팅 <li> 엘리먼트를 생성합니다
+    // 전달된 메시지와 클래스 이름으로 채팅 <li> 엘리먼트를 생성
     return (
       <li key={Math.random()} className={`chat ${className}`}>
         {className === "outgoing" ? <p>{message}</p> : <><span className="material-symbols-outlined">smart_toy</span><p>{message}</p></>}
@@ -41,29 +39,29 @@ function ChatBot() {
     const trimmedUserMessage = userMessage.trim();
     if (!trimmedUserMessage) return;
   
-    // 사용자의 메시지를 채팅 상자에 추가합니다
+    // 사용자의 메시지를 채팅 상자에 추가
     setChatbox((prevChatbox) => [...prevChatbox, createChatLi(trimmedUserMessage, "outgoing")]);
   
-    // 입력 텍스트 영역을 지웁니다
+    // 입력 텍스트 영역을 제거
     setUserMessage("");
 
-    // 입력 텍스트 영역의 높이를 초기화합니다
+    // 입력 텍스트 영역의 높이를 초기화
     const textareaElement = document.querySelector(".chat-input textarea");
     textareaElement.style.height = `${inputInitHeight}px`;
   
-    // 응답을 기다리는 동안 "Thinking..." 메시지를 표시합니다
+    // 응답을 기다리는 동안 "Thinking..." 메시지를 표시
     setChatbox((prevChatbox) => [...prevChatbox, createChatLi("Thinking...🤔", "incoming")]);
   
     try {
       const response = await generateResponse();
   
-      // "Thinking..." 메시지를 제거합니다
+      // "Thinking..." 메시지를 제거
       setChatbox((prevChatbox) => prevChatbox.slice(0, -1));
   
-      // 실제 응답을 표시합니다
+      // 실제 응답을 표시
       setChatbox((prevChatbox) => [...prevChatbox, createChatLi(response, "incoming")]);
     } catch (error) {
-      // "Thinking..." 메시지를 제거하고 오류 메시지를 표시합니다
+      // "Thinking..." 메시지를 제거하고 오류 메시지를 표시
       setChatbox((prevChatbox) => {
         const updatedChatbox = prevChatbox.slice(0, -1); // "Thinking..." 메시지 제거
         return [...updatedChatbox, createChatLi("이런! 오류가 발생했습니다.다시 시도해주세요.\n오류가 계속될 경우 관리자에게 문의해 주세요.😰", "incoming error")];
@@ -72,15 +70,15 @@ function ChatBot() {
   };
 
   const handleTextareaInput = (e) => {
-     // 내용에 따라 입력 텍스트 영역의 높이를 조절합니다
+     // 내용에 따라 입력 텍스트 영역의 높이를 조절
     e.target.style.height = `${inputInitHeight}px`;
     e.target.style.height = `${e.target.scrollHeight}px`;
-    // 입력된 메시지를 자동 줄바꿈합니다.
+    // 입력된 메시지를 자동 줄바꿈
     e.target.style.whiteSpace = 'pre-wrap';
   }
 
   const handleTextareaKeyDown = (e) => {
-    // Shift 키를 누르지 않고 Enter 키가 눌렸을 때 채팅을 처리합니다
+    // Shift 키를 누르지 않고 Enter 키가 눌렸을 때 채팅을 처리
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
       handleChat();
@@ -93,7 +91,7 @@ function ChatBot() {
   ]);
 
   useEffect(() => {
-    // 채팅 상자가 업데이트될 때 마다 가장 아래로 스크롤합니다
+    // 채팅 상자가 업데이트될 때 마다 가장 아래로 스크롤
     const chatboxElement = document.querySelector(".chatbox");
     chatboxElement.scrollTo(0, chatboxElement.scrollHeight);
   }, [chatbox]);
