fd17a8b123122d27c69221f6c7c389fabb67df18
JoMinGyu <jmingyu0317@gmail.com>
2024-03-12 22:20:11 +0900

    쇼핑몰 페이지

--- src/views/shop/Shop.js
+++ src/views/shop/Shop.js
@@ -1,14 +1,15 @@
+import React,{useEffect, useState} from 'react';
+import $ from 'jquery';
+import './Shop.css';
 import HeaderTop from "../component/headerTop/HeaderTop";
 import Header from "../component/header/Header";
 import Loader from "../component/loader/Loader";
 import Breadcumb from "../component/Breadcumb/Breadcumb";
 import ChatBot from "../component/chatBot/ChatBot";
-import $ from 'jquery';
-import React,{useEffect, useState} from 'react';
-import './Shop.css';
 import ShopListContainer from "./component/ShopListContainer";
-import ShopListMenu from "./component/ShopListMenu";
 import axios from 'axios'; //npm install axios
+import Banner from "./component/shopbanner";
+
 
 
 const ipAddress = '192.168.0.114';
@@ -54,8 +55,36 @@ function Shop() {
     }, [shopData]);
 
 
+    //리스트 정렬
 
+    const sortDataByPrice = (order) => {
+        const sortedData = [...shopDataAll];
+        sortedData.sort((a, b) => {
+          const priceA = parseFloat(a.price);
+          const priceB = parseFloat(b.price);
+          return order === 'asc' ? priceA - priceB : priceB - priceA;
+        });
+        setShopDataAll(sortedData);
+      };
+    
+    const handleSortChange = (e) => {
+    const selectedValue = e.target.value;
 
+    if (selectedValue === 'createdAt') {
+        setShopDataAll([...shopData]);// 디폴트값(정렬x)
+        } else if (selectedValue === 'descendingOrder') {
+            sortDataByPrice('desc');
+        } else if (selectedValue === 'ascendingOrder') {
+            sortDataByPrice('asc');
+        }
+    };
+
+    const [cartItems, setCartItems] = useState([]); // 장바구니에 담긴 상품 정보를 저장할 상태
+    // 상품을 장바구니에 추가하는 함수
+    const handleAddToCart = (item) => {
+        setCartItems([...cartItems, item]);
+    };
+    
   return (
     <div className="shop1">
         {/*헤더 위*/}
@@ -65,24 +94,22 @@ function Shop() {
         {/* 로딩 애니메이션*/}
         <Loader/>
 
-        <div className="footer-top-2 d-flex align-items-center">
+        <div className="footer-top-2-s d-flex align-items-center">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 col-md-6">
                         <div className="single-footer-top-box">
-                            <div className="footer-top-title">
-                                <h1>Purchase health item<br/>for your health</h1>
+                            <div className="footer-top-title-s">
+                                <h1>FitMe&Metree<br/>OnlineShop</h1>
+                                <h3>간편하게, 신선하게, 맛있게 - FitMe&Metree와 함께하는 온라인 쇼핑</h3>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-6 col-md-6">
                         <div className="single-footer-top-box">
-
-                            {/*
-                            <div className="footer-top-button">
-                                <a>Refresh Shop List</a>
+                            <div className="footer-top-button-s">
+                                <a href="/cart"> Go to cart</a>
                              </div>
-                             */}
                         </div>
                     </div>
                 </div>
@@ -90,27 +117,43 @@ function Shop() {
         </div>
 
         <div className="portfolio-area food-template">
-            {/*<ShopListMenu/>*/}
-
+            <Banner/>
             <div className="food-list-selector">
-                {/*
+                
                 <div>
-                    오늘의 전체 랭킹 순위!
+                <span style={{ fontWeight: 'bold',fontSize:'20px' }}>{shopDataAll.length}</span> 개의 상품
                 </div>
-                */}
+                
+                
+                <select id='sort' className='sortList' onChange={handleSortChange}>
+                    <option value='createdAt' defaultValue>최신순</option>
+                    <option value='descendingOrder'>높은 가격순</option>
+                    <option value='ascendingOrder'>낮은 가격순</option>
+                </select>
 
-                <button>
+                {/*
+                <button onClick={() => sortDataByPrice('desc')}>
                     높은 가격순
                 </button>
 
-                <button>
+                <button onClick={() => sortDataByPrice('asc')}>
                     낮은 가격순
-                </button>
+                </button >
+                */}
+
             </div>
             
             <div className="container">
-                {shopDataAll.map(item => (
-                    <ShopListContainer data={item} />
+                {shopDataAll.map((item, index) => (
+                    (index % 3 === 0) ? (
+                        <div className="row" key={index}>
+                            {shopDataAll.slice(index, index + 3).map((subItem, subIndex) => (
+                                <div className="col" key={index + subIndex}>
+                                    <ShopListContainer data={subItem} onAddToCart={handleAddToCart} />
+                                </div>
+                            ))}
+                        </div>
+                    ) : null
                 ))}
             </div>
         </div>
